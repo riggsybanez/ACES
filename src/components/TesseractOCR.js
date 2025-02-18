bd2497764d61dee415cce184c084872864811afe
riggsybanez <rybanez_220000001586@uic.edu.ph>
2025-02-18 14:51:34 +0800

    tesseract-js initial setup

---
+++ src/components/TesseractOCR.js
@@ -0,0 +1,15 @@
+// TesseractOCR.js
+import React from 'react';
+import Tesseract from 'tesseract.js';
+
+const TesseractOCR = () => {
+  // Placeholder for Tesseract setup and usage in the future
+  return (
+    <div>
+      <h1>Tesseract.js Setup</h1>
+      <p>Tesseract.js is ready to be used!</p>
+    </div>
+  );
+};
+
+export default TesseractOCR;
